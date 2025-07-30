f0ed27ecbdb703ec5a7a572f9197aec88fbc6184
Megan <liyuchenlyc2022@163.com>
2025-07-30 08:45:01 -0600

    feat: add dynamic post loading and convert post to html

--- main.js
+++ main.js
@@ -4,4 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
         });
+
+    const postsList = document.getElementById('posts-list');
+    postsData.forEach(post => {
+        const postLink = document.createElement('a');
+        if (post.url.endsWith('.md')) {
+            postLink.href = `post.html?post=${encodeURIComponent(post.url)}`;
+        } else {
+            postLink.href = post.url;
+            postLink.target = '_blank';
+        }
+        postLink.className = 'list-group-item list-group-item-action';
+        postLink.textContent = post.title;
+        postsList.appendChild(postLink);
+    });
 });
